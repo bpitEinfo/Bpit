c2b476ffda53c986d4783269910a1b30cf8ae70f
bpitEinfo <116291077+bpitEinfo@users.noreply.github.com>
2022-11-04 17:56:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,25 +4,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Home';
 import Signup from './src/Signup';
 import Login from './src/Login';
-import {firebase} from './config' ;
 import Profile from './src/Profile';
 
 const Stack = createNativeStackNavigator();
 function App() {
 
-  /*const [initializing,setInitializing] = useState(true);
-  const [user,setUser] =useState();
-  //Handle user State changes
-  function onAuthStatechange(user){
-    setUser(user);
-    if(initializing) setInitializing(false);
-  }
-  useEffect(() =>{
-    const subscriber = firebase.auth().onAuthStateChanged(onAuthStatechange);
-    return subscriber;
-  },[]);
-  if(initializing) return null;
-  if(!user)*/
   {
   return (
     <NavigationContainer>
@@ -30,11 +16,11 @@ function App() {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
-        
+        <Stack.Screen name="Profile" component={Profile} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
- // <Stack.Screen name="Profile" component={Profile} />
   }
 }
 
